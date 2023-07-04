61ae4b722a04294c45232376fe23b0a4ebdc279d
Stepan Lehkyi <slehk@softserveinc.com>
2023-07-04 16:25:04 +0300

    UC#7 Create complex unit tests

---
+++ UseCase7/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase7 {
+  public class Player {
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Experience { get; set; }
+    public List<int> Skills { get; set; }
+  }
+
+
+}
---
+++ UseCase7/PlayerAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase7 {
+  public class PlayerAnalyzer {
+    public double CalculateScore(List<Player> players) {
+      double score = 0;
+
+      foreach (var player in players) {
+        double skillsAverage = player.Skills.Sum() / (double)player.Skills.Count;
+        double contribution = player.Age * player.Experience * skillsAverage;
+
+        if (player.Age < 18) {
+          contribution *= 0.5;
+        }
+
+        if (player.Experience > 10) {
+          contribution *= 1.2;
+        }
+
+        score += contribution;
+      }
+
+      return score;
+    }
+  }
+
+}
